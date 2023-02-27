9408c653832d5c6449192996a738eba36246339e
Arystan <bekknop@gmail.com>
2023-02-27 16:38:38 +0600

    test

--- NetflixApp/MainViewController.swift
+++ NetflixApp/MainViewController.swift
@@ -5,6 +5,19 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        
+        
+        
     }
 }
 
+extension MainViewController {
+    
+    func setupView() {
+        
+    }
+    
+    func setConstraint() {
+        
+    }
+}
